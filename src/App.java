e6ebc89ed1c2dfa7c607615856a510e87cadc34e
RajnathMahendhra <rajnathmahendhra815@gmail.com>
2025-01-06 11:39:36 +0530

    first commit

--- src/App.java
+++ src/App.java
@@ -2,6 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
+        String str = "Rajnath";
+        
+    
 
     }
 }
